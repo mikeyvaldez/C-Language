5d6893bfc75beb7f5f398506a74e3765852a81eb
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-24 11:03:32 -0700

    test code

---
+++ displayName/src/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hi, my name is Mike!");
+    return 0;
+}
--- firstProgram/src/main.c
+++ firstProgram/src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-
+// The following code displays a hello world message to the console
 int main(int argc, char **argv){
 
     printf("Hello World!\n");
@@ -10,7 +10,7 @@ int main(int argc, char **argv){
 
 
 
-
+// The following code takes user input (integer) and displays a message along with the number the user put in
 // int main() {
 //     int myFavoriteNumber = 0;
 
