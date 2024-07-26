8d3a2bebe5f24fb8086fa9d0f86a34de338053e9
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-26 09:57:44 -0700

    float, double, _Bool and bool

---
+++ BasicDataTypes/src/dataTypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    float mike = 23.333;
+    double mike2 = 55.55555555555555;
+    double mike3 = 55.5555555555e+11; // scientific notation
+
+    _Bool boolVariable = 1;
+    bool myBoolean = true; // can only be used if #include <stdbool.h> is used
+
+    return 0;
+}
