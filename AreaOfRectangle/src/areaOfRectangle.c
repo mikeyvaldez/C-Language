096893312f8ab9540795cd955f9e2848ab551555
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-26 17:27:14 -0700

    Calculate the Area of a Rectangle

--- AreaOfRectangle/src/areaOfRectangle.c
+++ AreaOfRectangle/src/areaOfRectangle.c
@@ -1,26 +1,28 @@
 
 
-
+/*
+    This program calculates the perimeter of a rectangle
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 int main() {
 
+    // declaration of variables
     double width = 32.3;
     double height = 15.3;
     double perimeter = 0.0;
     double area = 0.0;
 
+    // perimeter calculation
     perimeter = 2.0 * (height + width);
     area = width * height;
 
-    printf("%c", '\n');
+    // displaying output
+    printf("%c", '\n'); // prints new line
     printf("Width is: %.2f, Height is: %.2f, Perimeter is: %f\n", width, height, perimeter);
     printf("Area is: %f\n", area);
-
 
     return 0;
 }
