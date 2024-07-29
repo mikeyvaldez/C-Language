97ba1d98fecf8bdc9f045fd66cb5d7232bc02b89
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-07-28 21:43:28 -0700

    demonstrate the while-loop

---
+++ ControlFlow/src/whileAndDoWhile.c
@@ -0,0 +1,54 @@
+
+// Program to introduce the while statement
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// int main()
+// {
+//     int count = 1;
+
+//     while (count <= 5)
+//     {
+//         printf("%i\n", count);
+//         ++count;
+//     }
+
+//     return 0;
+// }
+
+
+
+int main()
+{
+    int num = 0;
+    scanf("%d", &num);
+
+    while(num != -1)
+    {
+        /*loop actions*/
+        scanf("%d", &num);
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
